1bc5bb8692b11d2912bb8af9cb097df5a2e14075
Andrey Subbota <andrey@subbota.net>
2019-12-03 00:27:40 +0000

    Freeze cartodb versions

--- postgresql/cartodb.sh
+++ postgresql/cartodb.sh
@@ -72,7 +72,10 @@ for ext in cdb_dataservices_server; do
   psql -U "$PGUSER" dataservices_db -Atc "create extension if not exists \"${ext}\" CASCADE;";
 done
 echo "Configure dataservices_db'..."
-psql -U "$PGUSER" dataservices_db -c "SELECT CDB_Conf_SetConf('redis_metadata_config', '{\"redis_host\": \"$REDIS_HOST\", \"redis_port\": 6379, \"sentinel_master_id\": \"\", \"timeout\": 0.1, \"redis_db\": 5}');"
-psql -U "$PGUSER" dataservices_db -c "SELECT CDB_Conf_SetConf('redis_metrics_config', '{\"redis_host\": \"$REDIS_HOST\", \"redis_port\": 6379, \"sentinel_master_id\": \"\", \"timeout\": 0.1, \"redis_db\": 5}');"
-psql -U "$PGUSER" dataservices_db -c "SELECT CDB_Conf_SetConf('heremaps_conf', '{\"geocoder\": {\"app_id\": \"$HERE_APP_ID\", \"app_code\": \"$HERE_APP_CODE\", \"geocoder_cost_per_hit\": \"1\"}, \"isolines\" : {\"app_id\": \"$HERE_APP_ID\", \"app_code\": \"$HERE_APP_CODE\"}}');"
-psql -U "$PGUSER" dataservices_db -c "SELECT CDB_Conf_SetConf('mapbox_conf', '{\"routing\": {\"api_keys\": [\"$MAPBOX_API_TOKEN\"], \"monthly_quota\": 999999}, \"geocoder\": {\"api_keys\": [\"$MAPBOX_API_TOKEN\"], \"monthly_quota\": 999999}, \"matrix\": {\"api_keys\": [\"$MAPBOX_API_TOKEN\"], \"monthly_quota\": 1500000}}');"
+cat <<-CONFIG_SQL | tr -d '\n' | psql -U "$PGUSER" dataservices_db
+  SELECT CDB_Conf_SetConf('redis_metadata_config', '{"redis_host": "$REDIS_HOST", "redis_port": 6379, "sentinel_master_id": "", "timeout": 0.1, "redis_db": 5}');
+  SELECT CDB_Conf_SetConf('redis_metrics_config', '{"redis_host": "$REDIS_HOST", "redis_port": 6379, "sentinel_master_id": "", "timeout": 0.1, "redis_db": 5}');
+  SELECT CDB_Conf_SetConf('heremaps_conf', '{"geocoder": {"app_id": "$HERE_APP_ID", "app_code": "$HERE_APP_CODE", "geocoder_cost_per_hit": "1"}, "isolines" : {"app_id": "$HERE_APP_ID", "app_code": "$HERE_APP_CODE"}}');
+  SELECT CDB_Conf_SetConf('mapbox_conf', '{"routing": {"api_keys": ["$MAPBOX_API_TOKEN"], "monthly_quota": 999999}, "geocoder": {"api_keys": ["$MAPBOX_API_TOKEN"], "monthly_quota": 999999}, "matrix": {"api_keys": ["$MAPBOX_API_TOKEN"], "monthly_quota": 1500000}}');
+  SELECT CDB_Conf_SetConf('tomtom_conf', '{"routing": {"api_keys": ["$TOMTOM_API_TOKEN"], "monthly_quota": 999999}, "geocoder": {"api_keys": ["$TOMTOM_API_TOKEN"], "monthly_quota": 999999}, "isolines": {"api_keys": ["$TOMTOM_API_TOKEN"], "monthly_quota": 1500000}}');
+CONFIG_SQL
